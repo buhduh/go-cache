2546922d4c4d5c38daee69ee35fe14b7b2d5634d
dowen00 <Dale.Owens@bhnetwork.com>
2021-03-25 01:50:13 -0700

    redo, testing, and reaper

--- cache.go
+++ cache.go
@@ -3,7 +3,21 @@ package cache
 import (
 	"fmt"
 	"sync"
+	"time"
 )
+
+func NewInMemoryCache(lifetime *time.Duration) Cacher {
+	quit := make(chan int8)
+	cache := &inMemoryCache{
+		store:    new(sync.Map),
+		lifetime: lifetime,
+		quit:     quit,
+	}
+	//sooooo weird, there's GOT to be a better way to do this...
+	//circular reference...
+	cache.reaper = newReaper(cache, quit)
+	return cache
+}
 
 type Cacher interface {
 	Put(string, interface{}) error
@@ -11,19 +25,6 @@ type Cacher interface {
 	Clear() error
 	Remove(string) (interface{}, error)
 }
-
-type StringCacher interface {
-	Put(string, string) error
-	Get(string, ...string) (string, error)
-	Clear() error
-	Remove(string) (string, error)
-}
-
-type inMemoryCache struct {
-	store *sync.Map
-}
-
-type inMemoryStringCache inMemoryCache
 
 type ValueNotPresentError struct {
 	Key string
@@ -33,81 +34,88 @@ func (v ValueNotPresentError) Error() string {
 	return fmt.Sprintf("no value found for key '%s'", v.Key)
 }
 
-func NewInMemoryCacher() Cacher {
-	return &inMemoryCache{
-		store: new(sync.Map),
-	}
+type inMemoryCache struct {
+	lifetime *time.Duration
+	store    *sync.Map
+	reaper   *reaperTracker
+	quit     chan<- int8
 }
 
-func NewInMemoryStringCacher() StringCacher {
-	return &inMemoryStringCache{
-		store: new(sync.Map),
+type cacheElement struct {
+	value  *interface{}
+	ticker *time.Ticker
+}
+
+func (c *cacheElement) unPack() interface{} {
+	return *(c.value)
+}
+
+func (i *inMemoryCache) newElem(data interface{}) *cacheElement {
+	elem := new(cacheElement)
+	if i.lifetime != nil {
+		elem.ticker = time.NewTicker(*i.lifetime)
 	}
+	elem.value = &data
+	return elem
+}
+
+func castCacheElement(element interface{}) (*cacheElement, bool) {
+	toRet, ok := element.(*cacheElement)
+	return toRet, ok
 }
 
 func (i *inMemoryCache) Put(key string, data interface{}) error {
-	i.store.Store(key, data)
+	elem := i.newElem(data)
+	i.store.Store(key, elem)
+	go i.reaper.resetOrAddTimer(key, i.lifetime)
 	return nil
 }
 
 func (i *inMemoryCache) Get(key string, data ...interface{}) (interface{}, error) {
-	var toRet interface{}
+	var rawElem interface{}
 	if len(data) == 0 {
-		toRet, _ = i.store.Load(key)
+		rawElem, _ = i.store.Load(key)
 	} else if len(data) == 1 {
-		toRet, _ = i.store.LoadOrStore(key, data[0])
+		toStoreData := data[0]
+		elem := i.newElem(toStoreData)
+		rawElem, _ = i.store.LoadOrStore(key, elem)
 	} else {
 		return nil, fmt.Errorf(
 			"only one item can be cached with InMemoryCache.Get(), attempted to store %d items",
 			len(data),
 		)
 	}
-	if toRet == nil {
+	if rawElem == nil {
 		return nil, ValueNotPresentError{Key: key}
 	}
-	return toRet, nil
+	var elem *cacheElement
+	var ok bool
+	if elem, ok = castCacheElement(rawElem); !ok {
+		return nil, fmt.Errorf("could not cast cached value for key '%s' to a *cacheElement", key)
+	}
+	go i.reaper.resetOrAddTimer(key, i.lifetime)
+	return elem.unPack(), nil
 }
 
 func (i *inMemoryCache) Clear() error {
 	//I hope this actually recovers memory...
 	i.store = new(sync.Map)
+	close(i.quit)
+	newQuit := make(chan int8)
+	i.reaper = newReaper(i, newQuit)
+	i.quit = newQuit
 	return nil
 }
 
 func (i *inMemoryCache) Remove(key string) (interface{}, error) {
-	toRet, ok := i.store.LoadAndDelete(key)
+	rawElem, ok := i.store.LoadAndDelete(key)
+	go i.reaper.remove(key)
 	if !ok {
 		return nil, ValueNotPresentError{Key: key}
 	}
-	return toRet, nil
-}
-
-func (i *inMemoryStringCache) Put(key string, value string) error {
-	return ((*inMemoryCache)(i)).Put(key, value)
-}
-
-func (i *inMemoryStringCache) Get(key string, value ...string) (string, error) {
-	vals := make([]interface{}, len(value))
-	for i, v := range value {
-		vals[i] = v
+	var elem *cacheElement
+	if elem, ok = castCacheElement(rawElem); !ok {
+		return nil, fmt.Errorf("could not cast cached value for key '%s' to a *cachedElement", key)
 	}
-	toRet, err := ((*inMemoryCache)(i)).Get(key, vals...)
-	if err != nil {
-		return "", err
-	}
-	return toRet.(string), nil
-}
-
-func (i *inMemoryStringCache) Clear() error {
-	//I hope this actually recovers memory...
-	i.store = new(sync.Map)
-	return nil
-}
-
-func (i *inMemoryStringCache) Remove(key string) (string, error) {
-	toRet, err := ((*inMemoryCache)(i)).Remove(key)
-	if err != nil {
-		return "", err
-	}
-	return toRet.(string), nil
+	return elem.unPack(), nil
 }
--- cache_test.go
+++ cache_test.go
@@ -1,69 +1,159 @@
 package cache
 
 import (
+	"fmt"
 	"testing"
 )
 
 func TestCacher(t *testing.T) {
-	t.Run("type=inMemoryCache", testInMemoryStringCache)
+	t.Run("simple_functionality", simpleTest)
 }
 
-type dummyStruct struct {
-	number   int
-	words    string
-	contents *dummyStruct
+type dummyFoo struct {
+	foo int
 }
 
-func mkString(str string) *string {
-	return &str
+type dummyBar struct {
+	bar string
 }
 
-//TODO, need to test Get(sting, ...string)
-//perhaps add a Empty() bool?
-//test reset and clear
-var inMemoryStringCacheTests = []*struct {
-	name   string
-	keys   []string
-	values []*string
+type dummyReader struct{}
+
+func (d *dummyReader) Read([]byte) (int, error) {
+	return 10, nil
+}
+
+var p0, p1, p2 int = 0, 1, 2
+var s0, s1, s2 string = "0", "1", "2"
+
+//TODO need to test the reader too
+var simpleTests = []*struct {
+	name       string
+	valueTypes []string
+	values     [][]interface{}
 }{
 	{
-		"single key/value",
-		[]string{"foo"},
-		[]*string{mkString("bar")},
+		name:       "primitives",
+		valueTypes: []string{"int", "string"},
+		values: [][]interface{}{
+			[]interface{}{0, 1, 2},
+			[]interface{}{"0", "1", "2"},
+		},
 	},
 	{
-		"multiple values",
-		[]string{"foo", "bar", "baz"},
-		[]*string{mkString("blarg"), mkString("biff"), mkString("bar")},
+		name:       "primitive pointers",
+		valueTypes: []string{"*int", "*string"},
+		values: [][]interface{}{
+			[]interface{}{&p0, &p1, &p2},
+			[]interface{}{&s0, &s1, &s2},
+		},
+	},
+	{
+		name:       "composite types",
+		valueTypes: []string{"foo", "bar"},
+		values: [][]interface{}{
+			[]interface{}{dummyFoo{0}, dummyFoo{1}, dummyFoo{2}},
+			[]interface{}{dummyBar{"0"}, dummyBar{"1"}, dummyBar{"2"}},
+		},
+	},
+	{
+		name:       "composite pointer types",
+		valueTypes: []string{"*foo", "*bar"},
+		values: [][]interface{}{
+			[]interface{}{&dummyFoo{0}, &dummyFoo{1}, &dummyFoo{2}},
+			[]interface{}{&dummyBar{"0"}, &dummyBar{"1"}, &dummyBar{"2"}},
+		},
 	},
 }
 
-func testInMemoryStringCache(t *testing.T) {
-	for _, tCase := range inMemoryStringCacheTests {
-		myCache := NewInMemoryStringCacher()
-		for i, k := range tCase.keys {
-			err := myCache.Put(k, *tCase.values[i])
-			if err != nil {
+func simpleTest(t *testing.T) {
+	for _, tCase := range simpleTests {
+		for j, vType := range tCase.valueTypes {
+			if err := verifyValue(vType, tCase.values[j]); err != nil {
 				t.Errorf(
-					"should not have gotten an error for '%s' test, key: '%s', value '%s'",
-					tCase.name, k, *tCase.values[i],
-				)
-				break
-			}
-			cVal, err := myCache.Get(k)
-			if err != nil {
-				t.Errorf(
-					"should not have gotten an error for '%s' test, key: '%s', value '%s'",
-					tCase.name, k, *tCase.values[i],
-				)
-				break
-			}
-			if cVal != *tCase.values[i] {
-				t.Errorf(
-					"%s test: expected value '%s' does not match returned value, '%s', for Get()",
-					tCase.name, *tCase.values[i], cVal,
+					"%s test failed, type: '%s', error: '%s'",
+					tCase.name, vType, err,
 				)
 			}
 		}
 	}
+}
+
+func verifyValue(vType string, data []interface{}) error {
+	var ok bool
+	switch vType {
+	case "int":
+		var toCheck int
+		for i, v := range data {
+			if toCheck, ok = v.(int); !ok {
+				return fmt.Errorf("expected interface to be of type int")
+			} else if i != toCheck {
+				return fmt.Errorf("expected %d, got %d", i, toCheck)
+			}
+		}
+	case "string":
+		var toCheck string
+		for i, v := range data {
+			if toCheck, ok = v.(string); !ok {
+				return fmt.Errorf("expected interface to be of type string")
+			} else if fmt.Sprintf("%d", i) != toCheck {
+				return fmt.Errorf("expected %d, got %s", i, toCheck)
+			}
+		}
+	case "*int":
+		var toCheck *int
+		for i, v := range data {
+			if toCheck, ok = v.(*int); !ok {
+				return fmt.Errorf("expected interface to be of type *int")
+			} else if i != *toCheck {
+				return fmt.Errorf("expected %d, got %d", i, *toCheck)
+			}
+		}
+	case "*string":
+		var toCheck *string
+		for i, v := range data {
+			if toCheck, ok = v.(*string); !ok {
+				return fmt.Errorf("expected interface to be of type *string")
+			} else if fmt.Sprintf("%d", i) != *toCheck {
+				return fmt.Errorf("expected %d, got %s", i, *toCheck)
+			}
+		}
+	case "foo":
+		var toCheck dummyFoo
+		for i, v := range data {
+			if toCheck, ok = v.(dummyFoo); !ok {
+				return fmt.Errorf("expected interface to be of type dummyFoo")
+			} else if i != toCheck.foo {
+				return fmt.Errorf("expected %d, got %d", i, toCheck.foo)
+			}
+		}
+	case "bar":
+		var toCheck dummyBar
+		for i, v := range data {
+			if toCheck, ok = v.(dummyBar); !ok {
+				return fmt.Errorf("expected interface to be of type dummyBar")
+			} else if fmt.Sprintf("%d", i) != toCheck.bar {
+				return fmt.Errorf("expected %d, got %s", i, toCheck.bar)
+			}
+		}
+	case "*foo":
+		var toCheck *dummyFoo
+		for i, v := range data {
+			if toCheck, ok = v.(*dummyFoo); !ok {
+				return fmt.Errorf("expected interface to be of type *dummyFoo")
+			} else if i != (*toCheck).foo {
+				return fmt.Errorf("expected %d, got %d", i, toCheck.foo)
+			}
+		}
+	case "*bar":
+		var toCheck *dummyBar
+		for i, v := range data {
+			if toCheck, ok = v.(*dummyBar); !ok {
+				return fmt.Errorf("expected interface to be of type *dummyBar")
+			} else if fmt.Sprintf("%d", i) != (*toCheck).bar {
+				return fmt.Errorf("expected %d, got %s", i, toCheck.bar)
+			}
+		}
+	}
+	return nil
 }
