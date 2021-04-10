2fc449b7a42802062fb9ee3f3a1cf81e1865dece
dowen00 <Dale.Owens@bhnetwork.com>
2021-04-10 14:26:41 -0700

    works

--- spike/go.mod
+++ spike/go.mod
@@ -1,3 +1,5 @@
 module spike
 
 go 1.16
+
+require github.com/buhduh/go-cache v0.0.0-20210410210501-2a2bf2aa6bd0
--- spike/main.go
+++ spike/main.go
@@ -1,20 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	cache "github.com/buhduh/go-cache"
+	"github.com/buhduh/go-cache/datahandler"
+	"github.com/buhduh/go-cache/invalidator"
+	"time"
+)
 
 func main() {
-	foo := make(chan int, 1000)
-	for i := 0; i < 1000; i++ {
-		foo <- i
-	}
-loop:
-	for {
-		select {
-		case tmp := <-foo:
-			fmt.Printf("%d\n", tmp)
-		default:
-			fmt.Printf("default")
-			break loop
-		}
-	}
+	exp, _ := time.ParseDuration("1s")
+	myCache := cache.NewCache(
+		datahandler.NewInMemoryDataHandler(),
+		invalidator.NewTimedInvalidator(exp),
+	)
+	myCache.Put("key", "key")
+	key, _ := myCache.Put("key", "foo")
+	fmt.Printf("should be 'key': '%s'\n", key.(string))
+	key, _ = myCache.Get("key")
+	fmt.Printf("should be 'foo': '%s'\n", key.(string))
+	twoSec, _ := time.ParseDuration("2s")
+	time.Sleep(twoSec)
+	gone, err := myCache.Get("key")
+	fmt.Printf("should be not found: '%s'", err)
+	fmt.Printf("should be nil: %v", gone)
 }
